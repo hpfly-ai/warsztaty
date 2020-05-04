021adb6993443a96cbb9f9a1adb3b11b2ae9f46e
hpfly-ai <63752476+hpfly-ai@users.noreply.github.com>
2020-05-04 16:08:29 +0200

    Warsztat 2 Etap 1 - Done

--- Warsztat2/trunk/library/test/ClientTest.cpp
+++ Warsztat2/trunk/library/test/ClientTest.cpp
@@ -1,9 +1,11 @@
 #include <boost/test/unit_test.hpp>
+namespace tt = boost::test_tools;
 
 BOOST_AUTO_TEST_SUITE(TestSuiteClient)
 
 BOOST_AUTO_TEST_CASE(AssertionsTests) {
+        BOOST_TEST(1.0/3.0 == 0.333, tt::tolerance(0.003));
         BOOST_TEST(true);
-}
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
